974678b4254f108e415e699c3974e16cfc78efc4
Alexandre-BARBIER <100125903+Alexandre-BARBIER@users.noreply.github.com>
2023-05-11 23:46:32 +0200

    Add files via upload

---
+++ fish/functions/fish_right_prompt.fish
@@ -0,0 +1,41 @@
+function fish_right_prompt --description 'Write out the prompt'
+    set -l last_pipestatus $pipestatus
+    set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+
+    if not set -q __fish_git_prompt_show_informative_status
+        set -g __fish_git_prompt_show_informative_status 1
+    end
+    if not set -q __fish_git_prompt_hide_untrackedfiles
+        set -g __fish_git_prompt_hide_untrackedfiles 1
+    end
+    if not set -q __fish_git_prompt_showupstream
+        set -g __fish_git_prompt_showupstream informative
+    end
+    if not set -q __fish_git_prompt_color_prefix
+    	set -g __fish_git_prompt_color_prefix white
+    end
+    if not set -q __fish_git_prompt_color_suffix
+       set -g __fish_git_prompt_color_suffix white
+    end
+    if not set -q __fish_git_prompt_color_dirtystate
+       set -g __fish_git_prompt_color_dirtystate blue
+    end
+    if not set -q __fish_git_prompt_color_stagedstate
+        set -g __fish_git_prompt_color_stagedstate yellow
+    end
+    if not set -q __fish_git_prompt_color_invalidstate
+        set -g __fish_git_prompt_color_invalidstate red
+    end
+    if not set -q __fish_git_prompt_color_untrackedfiles
+        set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+    end
+    if not set -q __fish_git_prompt_color_cleanstate
+        set -g __fish_git_prompt_color_cleanstate green --bold
+    end
+
+
+    printf '%s ' (fish_vcs_prompt)
+
+
+end
+
